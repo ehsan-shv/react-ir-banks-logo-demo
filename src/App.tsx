c4d58e1659ac0041a506802b15434d5c9f37692e
eh. sh <sh.ehsan74.es@gmail.com>
2022-05-07 22:47:37 +0430

    done

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,17 @@
 import React, { useState } from 'react';
-import logo from './logo.svg';
 import './App.css';
-import BankIcon from './components/BankIcon/BankIcon';
+import { BankIcon } from 'react-ir-banks-logo';
 
 function App() {
   const [digits, setDigits] = useState('');
+  const [name, setName] = useState('');
   return (
     <div className="App">
       <header className="App-header">
+        <label>شماره کارت:</label>
         <input type="text" value={digits} onChange={(e) => setDigits(e.target.value)} />
-        <BankIcon digits={digits} onFindBankName={(name) => console.log(name)} />
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-          Learn React
-        </a>
+        <BankIcon digits={digits} size="200px" margin="100px" onFindBankName={(name: string) => setName(name)} />
+        <h1>{name}</h1>
       </header>
     </div>
   );
